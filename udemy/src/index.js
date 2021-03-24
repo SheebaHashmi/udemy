bed4c5d0ee1a549f228117faed25b40084a3b2d3
SheebaHashmi <64197466+SheebaHashmi@users.noreply.github.com>
2021-03-24 19:25:37 +0500

    Update index.js

--- udemy/src/index.js
+++ udemy/src/index.js
@@ -1,19 +1,44 @@
 // import React libraries 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker'
+import CommentDetails from './CommentDetails';
+import ApproveCard from './ApproveCard';
 
 //Create React component
 const App = () => {
-    const button = {text: 'hello'}
-    const style = {backgroundColor: 'blue', color:'white'};
+  
     return (
-        <div>
-            <label className = 'label' htmlFor='name'>Enter name:</label>
-            <input id= 'name' type='text'/>
-            <button style={style}>{button.text}</button>
-        </div>
+      <div className= "ui container comments">
+          <ApproveCard>Are you sure?</ApproveCard>
+          <ApproveCard>
+            <CommentDetails
+            author = "Sam"
+            timeAgo = "Today at 3:20PM"
+            comment = "Hey! its me, Sam."
+            avatar = {faker.image.image()}
+            />
+          </ApproveCard>
+          <ApproveCard>
+          <CommentDetails 
+            author = "Bill"
+            timeAgo = "Today at 7:20PM"
+            comment = "Hey! its me, Bill."
+            avatar = {faker.image.image()}
+            />
+          </ApproveCard>
+          <ApproveCard>
+          <CommentDetails
+            author = "Hair"
+            timeAgo = "Today at 4:10PM"
+            comment = "Hey! its me, Hair."
+            avatar = {faker.image.image()}
+        />
+          </ApproveCard>
+          
+      </div>  
 
-    )
+    );
 };
 //Display React components
 ReactDOM.render(<App/>,document.querySelector('#root'));
