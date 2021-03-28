4881a18682a231862558ed963405a94854af223f
SheebaHashmi <64197466+SheebaHashmi@users.noreply.github.com>
2021-03-28 08:25:15 +0500

    starting project Feedback from the user and fetching data

--- udemy/src/index.js
+++ udemy/src/index.js
@@ -1,39 +1,8 @@
 // import React libraries 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
+import App from './components/App';
 
-//Create React component
 
-class App extends React.Component{
-   state = {lat:null, errorMessage: ''}
-
-    componentDidMount(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({lat:position.coords.latitude}), 
-            (err) => this.setState({errorMessage: err.message})
-            
-        );
-    };
-    renderContent(){
-        if(this.state.errorMessage && !this.state.lat){
-            return <div>Error:{this.state.errorMessage}</div>;
-         }
-         if(!this.state.errorMessage && this.state.lat){
-             return <div><SeasonDisplay lat = {this.state.lat}/></div>;
-         }
-         return <Spinner message = 'Please accept location request.'/>;
-     };
-
-    
-    render(){
-      return(
-        <div className="border red">
-            {this.renderContent()}
-        </div>
-      );
-    };
-}
 //Display React components
 ReactDOM.render(<App/>,document.querySelector('#root'));
