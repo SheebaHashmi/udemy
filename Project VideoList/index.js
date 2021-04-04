ca2e446211148c924b581bd1e2aed0b5ce755331
SheebaHashmi <64197466+SheebaHashmi@users.noreply.github.com>
2021-04-04 14:39:04 +0500

    source code of Project VideoList

---
+++ Project VideoList/index.js
@@ -0,0 +1,8 @@
+// import React libraries 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+//Display React components
+ReactDOM.render(<App/>,document.querySelector('#root'));
